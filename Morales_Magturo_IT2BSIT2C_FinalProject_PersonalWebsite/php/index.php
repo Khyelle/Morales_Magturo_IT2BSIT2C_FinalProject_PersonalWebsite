a1367b465e96003ceff1b43c8b2fc013ab76779d
Khyelle <101545138+Khyelle@users.noreply.github.com>
2022-05-30 15:37:54 +0800

    Add files via upload

---
+++ Morales_Magturo_IT2BSIT2C_FinalProject_PersonalWebsite/php/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Portfolio</title>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+    
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+    <h1>Upload yuor images here!</h1>
+    <div class="xx">
+        <input class="input" type="file" name="file">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <button class="btn" type="submit" name="submit">UPLOAD</button>
+</div>
+
+<div class="follow">
+            <h3> follow us in:</h3>
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-github"></a>
+        </div>
+</form>
+</body>
+</html>
+
+<?php
+
+?>
